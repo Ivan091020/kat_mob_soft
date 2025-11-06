6a5159539254a060c6422f956e46c7f7c26e9adc
Ivan091020 <zhurbaivan@vk.com>
2025-11-06 03:11:31 +0200

    5

--- kat_mob_soft/Controllers/HomeController.cs
+++ kat_mob_soft/Controllers/HomeController.cs
@@ -33,6 +33,13 @@ namespace kat_mob_soft.Controllers
         {
             return View();
         }
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+      
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
